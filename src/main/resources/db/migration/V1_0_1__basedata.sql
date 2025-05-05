c36c7f661ade2880542347a13b1b4bb761579c4c
Marcus Fihlon <marcus@fihlon.swiss>
2025-05-05 08:24:42 +0200

    fix(db): incorrect table name breaks the build
    Signed-off-by: Marcus Fihlon <marcus@fihlon.swiss>

--- src/main/resources/db/migration/V1_0_1__basedata.sql
+++ src/main/resources/db/migration/V1_0_1__basedata.sql
@@ -1,5 +1,5 @@
 -- [jooq ignore start]
 
-INSERT INTO `roles` (`id`, `key`) VALUES (1, 'admin'), (2, 'organizer'), (3, 'member');
+INSERT INTO `role` (`id`, `key`) VALUES (1, 'admin'), (2, 'organizer'), (3, 'member');
 
 -- [jooq ignore stop]
