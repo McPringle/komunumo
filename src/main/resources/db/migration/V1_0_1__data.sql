4fa59efb6d03f0abfa55c6e70d3dfb2f85b6011d
Marcus Fihlon <marcus@fihlon.swiss>
2025-05-17 16:41:01 +0200

    refactor(db): remove backticks from SQL identifiers for compatibility
    Removes MySQL-style backticks from column and table names to improve compatibility with other SQL dialects. No functional or structural changes to the schema.
    
    Signed-off-by: Marcus Fihlon <marcus@fihlon.swiss>

--- src/main/resources/db/migration/V1_0_1__data.sql
+++ src/main/resources/db/migration/V1_0_1__data.sql
@@ -1,6 +1,6 @@
 -- [jooq ignore start]
 
-INSERT INTO `mail_template` (`id`, `language`, `subject`, `markdown`)
+INSERT INTO mail_template (id, language, subject, markdown)
 VALUES ('USER_LOGIN_CONFIRMATION','DE','Deine Anmeldung bei Komunumo', 'Bitte klicke auf den folgenden Link, um dich bei Komunumo anzumelden:\n${login_link}'),
        ('USER_LOGIN_CONFIRMATION','EN','Your Login Request at Komunumo', 'Please click on the following link to log in to Komunumo:\n${login_link}');
 
