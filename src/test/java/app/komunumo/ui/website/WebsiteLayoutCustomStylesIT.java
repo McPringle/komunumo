e055be764f987db85999133026e59659c25d68ed
Marcus Fihlon <marcus@fihlon.swiss>
2025-07-19 21:41:36 +0200

    test: refactor tests to use shared HttpTestServer
    Signed-off-by: Marcus Fihlon <marcus@fihlon.swiss>

--- src/test/java/app/komunumo/ui/website/WebsiteLayoutCustomStylesIT.java
+++ src/test/java/app/komunumo/ui/website/WebsiteLayoutCustomStylesIT.java
@@ -18,84 +18,21 @@
 package app.komunumo.ui.website;
 
 import app.komunumo.ui.BrowserTest;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.InetSocketAddress;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.http.HttpClient;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class WebsiteLayoutCustomStylesIT extends BrowserTest {
 
-    private static final AtomicInteger counter = new AtomicInteger(0);
-    private static HttpServer server;
-
     @Override
     protected String[] getProperties() {
         return new String[] {
-                "--komunumo.custom.styles=http://localhost:8082/styles.css"
+                "--komunumo.custom.styles=http://localhost:8082/custom-styles/styles.css"
         };
-    }
-
-    @BeforeAll
-    static void startServer() throws IOException {
-        final var cssFile = Path.of("src/test/resources/custom-styles/styles.css");
-
-        final HttpHandler handler = exchange -> {
-            if (exchange.getRequestURI().getPath().equals("/styles.css")) {
-                counter.incrementAndGet();
-                final byte[] bytes = Files.readAllBytes(cssFile);
-                exchange.getResponseHeaders().set("Content-Type", "text/css");
-                exchange.sendResponseHeaders(200, bytes.length);
-                try (OutputStream os = exchange.getResponseBody()) {
-                    os.write(bytes);
-                }
-            } else {
-                exchange.sendResponseHeaders(404, -1);
-            }
-        };
-
-        server = HttpServer.create(new InetSocketAddress(8082), 0);
-        server.createContext("/", handler);
-        server.start();
-    }
-
-    @AfterAll
-    static void stopServer() {
-        server.stop(0);
-    }
-
-    private void testWebserver() throws URISyntaxException, IOException, InterruptedException {
-        try (var client = HttpClient.newHttpClient()) {
-            var response = client.send(
-                    java.net.http.HttpRequest.newBuilder()
-                            .uri(new URI("http://localhost:8082/styles.css"))
-                            .build(),
-                    java.net.http.HttpResponse.BodyHandlers.ofString()
-            );
-
-            // Test
-            assertThat(response.statusCode()).isEqualTo(200);
-            assertThat(response.body()).startsWith(":root {");
-        }
     }
 
     @Test
-    void testCustomStylesLoadedAndApplied() throws InterruptedException, URISyntaxException, IOException {
-        testWebserver();
-        assertThat(counter.get()).isEqualTo(1);
-
+    void testCustomStylesLoadedAndApplied() {
         final var page = getPage();
 
         page.navigate("http://localhost:8081/");
--- src/test/java/app/komunumo/util/DownloadUtilTest.java
+++ src/test/java/app/komunumo/util/DownloadUtilTest.java
@@ -17,72 +17,30 @@
  */
 package app.komunumo.util;
 
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.InetSocketAddress;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class DownloadUtilTest {
 
-    private static HttpServer server;
-
-    @BeforeAll
-    static void startServer() throws IOException {
-        final var cssFile = Path.of("src/test/resources/custom-styles/styles.css");
-
-        final HttpHandler handler = exchange -> {
-            final var path = exchange.getRequestURI().getPath();
-            if (path.equals("/styles.css")) {
-                final byte[] bytes = Files.readAllBytes(cssFile);
-                exchange.getResponseHeaders().set("Content-Type", "text/css");
-                exchange.sendResponseHeaders(200, bytes.length);
-                try (OutputStream os = exchange.getResponseBody()) {
-                    os.write(bytes);
-                }
-            } else if (path.equals("/99")) {
-                exchange.sendResponseHeaders(99, -1);
-            } else {
-                exchange.sendResponseHeaders(404, -1);
-            }
-        };
-
-        server = HttpServer.create(new InetSocketAddress(8082), 0);
-        server.createContext("/", handler);
-        server.start();
-    }
-
-    @AfterAll
-    static void stopServer() {
-        server.stop(0);
-    }
-
     @Test
     void getString() throws Exception {
-        final String string = DownloadUtil.getString("http://localhost:8082/styles.css").trim();
+        final String string = DownloadUtil.getString("http://localhost:8082/custom-styles/styles.css").trim();
         assertThat(string).startsWith(":root {").endsWith("}");
     }
 
     @Test
     void downloadFileSuccess() {
-        final var path = DownloadUtil.downloadFile("http://localhost:8082/styles.css");
+        final var path = DownloadUtil.downloadFile("http://localhost:8082/custom-styles/styles.css");
         assertThat(path).isNotNull().exists();
     }
 
     @ParameterizedTest
     @ValueSource(strings = {
             "http://localhost:8082/99",
-            "http://localhost:8082/404",
+            "http://localhost:8082/non-existing",
             "http://localhost:8888/"
     })
     void shouldReturnNullForInvalidOrUnreachableUrls(final String url) {
