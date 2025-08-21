4813ba08f8dc89f2f7c1934c2d63e1a73dcc0221
Marcus Fihlon <marcus@fihlon.swiss>
2025-08-21 17:01:47 +0200

    refactor(join): tests for `ParticipationService`
    Part of: #218
    
    Signed-off-by: Marcus Fihlon <marcus@fihlon.swiss>

--- src/test/java/app/komunumo/data/service/ParticipationServiceIT.java
+++ src/test/java/app/komunumo/data/service/ParticipationServiceIT.java
@@ -22,24 +22,15 @@ import app.komunumo.data.dto.UserRole;
 import app.komunumo.data.dto.UserType;
 import app.komunumo.ui.IntegrationTest;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.ZonedDateTime;
 import java.util.Locale;
-import java.util.regex.Pattern;
 
-import static com.icegreen.greenmail.util.GreenMailUtil.getBody;
-import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.awaitility.Awaitility.await;
 
 class ParticipationServiceIT extends IntegrationTest {
-
-    private static final Pattern EXTRACT_CODE_FROM_BODY =
-            Pattern.compile("\\bcode\\b[\\p{Punct}\\s]*\"(\\d{6})\"",
-                    Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
 
     @Autowired
     private @NotNull ParticipationService participationService;
@@ -50,104 +41,79 @@ class ParticipationServiceIT extends IntegrationTest {
     @Autowired
     private @NotNull UserService userService;
 
-//    @Test
-//    void requestVerificationCode() {
-//        final var event = eventService.getUpcomingEventsWithImage().getFirst().event();
-//        final var locale = Locale.ENGLISH;
-//
-//        assertThat(participationService.requestVerificationCode(event, "", locale)).isFalse();
-//        assertThat(participationService.requestVerificationCode(event, "test@komunumo.app", locale)).isTrue();
-//
-//        await().atMost(2, SECONDS).untilAsserted(() -> {
-//            final var receivedMessage = greenMail.getReceivedMessages()[0];
-//            assertThat(receivedMessage.getAllRecipients()[0]).hasToString("test@komunumo.app");
-//            assertThat(receivedMessage.getSubject()).isEqualTo("Confirm your event join request");
-//
-//            final var body = getBody(receivedMessage);
-//            assertThat(body)
-//                    .doesNotContain("${eventTitle}")
-//                    .doesNotContain("${verificationCode}")
-//                    .doesNotContain("${verificationLink}")
-//                    .contains(event.title());
-//
-//            final var code = extractCodeFromBody(body);
-//            verifyGoodCode(code);
-//        });
-//    }
-//
-//    @Test
-//    void joinEventExistingUser() {
-//        assertThat(participationService.getParticipations()).isEmpty();
-//
-//        final var email = "test@komunumo.app";
-//        assertThat(userService.getUserByEmail(email)).isEmpty();
-//
-//        userService.storeUser(new UserDto(null, null, null, null, email, "", "",
-//                null, UserRole.USER, UserType.LOCAL, null));
-//        final var user = userService.getUserByEmail(email).orElseThrow();
-//        assertThat(user).isNotNull();
-//
-//        final var event = eventService.getUpcomingEventsWithImage().getFirst().event();
-//        assertThat(event).isNotNull();
-//
-//        final var locale = Locale.ENGLISH;
-//        assertThat(participationService.joinEvent(event, email, locale)).isTrue();
-//
-//        final var participations = participationService.getParticipations();
-//        assertThat(participations).hasSize(1);
-//
-//        final var participation = participations.getFirst();
-//        assertThat(participation).isNotNull().satisfies(testee -> {
-//            assertThat(testee.eventId()).isEqualTo(event.id());
-//            assertThat(testee.userId()).isEqualTo(user.id());
-//            assertThat(testee.registered()).isNotNull();
-//            assertThat(testee.registered()).isBeforeOrEqualTo(ZonedDateTime.now());
-//        });
-//
-//        assertThat(participationService.deleteParticipation(participation)).isTrue();
-//        assertThat(participationService.deleteParticipation(participation)).isFalse();
-//        assertThat(participationService.getParticipations()).isEmpty();
-//
-//        assertThat(userService.deleteUser(user)).isTrue();
-//        assertThat(userService.deleteUser(user)).isFalse();
-//    }
-//
-//    @Test
-//    void joinEventAnonymousUser() {
-//        assertThat(participationService.getParticipations()).isEmpty();
-//
-//        final var email = "test@komunumo.app";
-//        assertThat(userService.getUserByEmail(email)).isEmpty();
-//
-//        final var event = eventService.getUpcomingEventsWithImage().getFirst().event();
-//        assertThat(event).isNotNull();
-//
-//        final var locale = Locale.ENGLISH;
-//        assertThat(participationService.joinEvent(event, email, locale)).isTrue();
-//
-//        // try to join again with the same email
-//        assertThat(participationService.joinEvent(event, email, locale)).isTrue();
-//
-//        final var participations = participationService.getParticipations();
-//        assertThat(participations).hasSize(1);
-//
-//        final var user = userService.getUserByEmail(email).orElseThrow();
-//        assertThat(user).isNotNull();
-//
-//        final var participation = participations.getFirst();
-//        assertThat(participation).isNotNull().satisfies(testee -> {
-//            assertThat(testee.eventId()).isEqualTo(event.id());
-//            assertThat(testee.userId()).isEqualTo(user.id());
-//            assertThat(testee.registered()).isNotNull();
-//            assertThat(testee.registered()).isBeforeOrEqualTo(ZonedDateTime.now());
-//        });
-//
-//        assertThat(participationService.deleteParticipation(participation)).isTrue();
-//        assertThat(participationService.deleteParticipation(participation)).isFalse();
-//        assertThat(participationService.getParticipations()).isEmpty();
-//
-//        assertThat(userService.deleteUser(user)).isTrue();
-//        assertThat(userService.deleteUser(user)).isFalse();
-//    }
+    @Test
+    void joinEventExistingUser() {
+        assertThat(participationService.getParticipations()).isEmpty();
+
+        final var email = "test@komunumo.app";
+        assertThat(userService.getUserByEmail(email)).isEmpty();
+
+        userService.storeUser(new UserDto(null, null, null, null, email, "", "",
+                null, UserRole.USER, UserType.LOCAL, null));
+        final var user = userService.getUserByEmail(email).orElseThrow();
+        assertThat(user).isNotNull();
+
+        final var event = eventService.getUpcomingEventsWithImage().getFirst().event();
+        assertThat(event).isNotNull();
+
+        final var locale = Locale.ENGLISH;
+        assertThat(participationService.joinEvent(event, email, locale)).isTrue();
+
+        final var participations = participationService.getParticipations();
+        assertThat(participations).hasSize(1);
+
+        final var participation = participations.getFirst();
+        assertThat(participation).isNotNull().satisfies(testee -> {
+            assertThat(testee.eventId()).isEqualTo(event.id());
+            assertThat(testee.userId()).isEqualTo(user.id());
+            assertThat(testee.registered()).isNotNull();
+            assertThat(testee.registered()).isBeforeOrEqualTo(ZonedDateTime.now());
+        });
+
+        assertThat(participationService.deleteParticipation(participation)).isTrue();
+        assertThat(participationService.deleteParticipation(participation)).isFalse();
+        assertThat(participationService.getParticipations()).isEmpty();
+
+        assertThat(userService.deleteUser(user)).isTrue();
+        assertThat(userService.deleteUser(user)).isFalse();
+    }
+
+    @Test
+    void joinEventAnonymousUser() {
+        assertThat(participationService.getParticipations()).isEmpty();
+
+        final var email = "test@komunumo.app";
+        assertThat(userService.getUserByEmail(email)).isEmpty();
+
+        final var event = eventService.getUpcomingEventsWithImage().getFirst().event();
+        assertThat(event).isNotNull();
+
+        final var locale = Locale.ENGLISH;
+        assertThat(participationService.joinEvent(event, email, locale)).isTrue();
+
+        // try to join again with the same email
+        assertThat(participationService.joinEvent(event, email, locale)).isTrue();
+
+        final var participations = participationService.getParticipations();
+        assertThat(participations).hasSize(1);
+
+        final var user = userService.getUserByEmail(email).orElseThrow();
+        assertThat(user).isNotNull();
+
+        final var participation = participations.getFirst();
+        assertThat(participation).isNotNull().satisfies(testee -> {
+            assertThat(testee.eventId()).isEqualTo(event.id());
+            assertThat(testee.userId()).isEqualTo(user.id());
+            assertThat(testee.registered()).isNotNull();
+            assertThat(testee.registered()).isBeforeOrEqualTo(ZonedDateTime.now());
+        });
+
+        assertThat(participationService.deleteParticipation(participation)).isTrue();
+        assertThat(participationService.deleteParticipation(participation)).isFalse();
+        assertThat(participationService.getParticipations()).isEmpty();
+
+        assertThat(userService.deleteUser(user)).isTrue();
+        assertThat(userService.deleteUser(user)).isFalse();
+    }
 
 }
