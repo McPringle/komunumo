b523cdeb192e25a38142df197aa70bd2fe187cbf
Marcus Fihlon <marcus@fihlon.swiss>
2025-10-08 15:45:55 +0200

    test(logout): add missing test for `LogoutView`
    Signed-off-by: Marcus Fihlon <marcus@fihlon.swiss>

---
+++ src/test/java/app/komunumo/ui/views/login/LogoutViewIT.java
@@ -0,0 +1,47 @@
+/*
+ * Komunumo - Open Source Community Manager
+ * Copyright (C) Marcus Fihlon and the individual contributors to Komunumo.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package app.komunumo.ui.views.login;
+
+import app.komunumo.data.dto.UserRole;
+import app.komunumo.ui.IntegrationTest;
+import app.komunumo.ui.components.ConfirmationDialog;
+import app.komunumo.util.SecurityUtil;
+import com.github.mvysny.kaributesting.v10.MockVaadin;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import org.junit.jupiter.api.Test;
+
+import static com.github.mvysny.kaributesting.v10.LocatorJ._click;
+import static com.github.mvysny.kaributesting.v10.LocatorJ._get;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LogoutViewIT extends IntegrationTest {
+
+    @Test
+    void logoutView() {
+        final var user = getTestUser(UserRole.USER);
+        login(user);
+        MockVaadin.clientRoundtrip();
+        assertThat(SecurityUtil.isLoggedIn()).isTrue();
+
+        UI.getCurrent().navigate(LogoutView.class);
+        MockVaadin.clientRoundtrip();
+        assertThat(SecurityUtil.isLoggedIn()).isFalse();
+    }
+
+}
